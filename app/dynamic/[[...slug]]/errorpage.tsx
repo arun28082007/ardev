dbe755052ce815fe29cde00a1a4cf98a7f7db576
arunanand0807 <arunanand0807@gmail.com>
2024-10-29 11:44:40 +0530

    Changes to be committed: 	renamed:    app/dynamic/[...slug]/page.tsx -> app/dynamic/[[...slug]]/errorpage.tsx 	new file:   app/dynamic/[[...slug]]/page.tsx
    dynamic withoit slug

--- app/dynamic/[[...slug]]/errorpage.tsx
+++ app/dynamic/[[...slug]]/errorpage.tsx
@@ -2,7 +2,8 @@ export default function slug({ params }: { params: { slug: string } }) {
     return (
         <div className="flex justify-center items-center h-screen">
             <h1>{params.slug}</h1>
-            <h1>bholu</h1>
+            <h1>dynamic</h1>
         </div>
     );
 }
+
